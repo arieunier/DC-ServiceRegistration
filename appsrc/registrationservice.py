af60c722546b1ba47b15ae4c98499642f6887397
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2020-12-08 21:54:56 +0100

    dclibs

--- appsrc/registrationservice.py
+++ appsrc/registrationservice.py
@@ -1,5 +1,5 @@
 import ujson
-from dc-libs import logs, queuer, rabbitmq_utils, config, rediscache, sfapi, postgres, utils
+from dclibs import logs, queuer, rabbitmq_utils, config, rediscache, sfapi, postgres, utils
 LOGGER = logs.LOGGER
 
 SERVICE_NAME='registrationservice'
